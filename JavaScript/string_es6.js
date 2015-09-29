b6a62dc02440a8fdaa5c5dd451ae13de49fcf782
wannabewize <wannabewize@gmail.com>
2015-09-30 03:12:36 +0900

    JavaScript ES6, String

---
+++ JavaScript/string_es6.js
@@ -0,0 +1,17 @@
+var a = 5;
+var b = 10;
+
+var str1 = a + ' + ' + b + ' = ' + (a + b);
+var str2 = `${a} + ${b} = ${a + b}`;
+console.log(str1);
+console.log(str2);
+
+
+
+var str3 = 'Hello \nJavascript';
+var str4 = `Hello 
+Multiline
+String`;
+
+console.log(str3);
+console.log(str4);
