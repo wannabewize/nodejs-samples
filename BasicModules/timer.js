845f5319fdab417cb1ff9a31b46e734181b7c3c8
Lee Jaehoon <wannabewize@gmail.com>
2015-10-10 23:24:17 +0900

    timer, interval sample

--- BasicModules/timer.js
+++ BasicModules/timer.js
@@ -1,10 +1,22 @@
+// Timer Sample
 function sayHello() {
    console.log('Hello World');
 }
 
+console.log('== Timer Example.');
 setTimeout(sayHello, 3);
-console.log('Timer Example.');
 
 // t 취소
 var t = setTimeout(sayHello, 10);
 clearTimeout(t);
+
+
+// Interval Sample
+var count = 5;
+function sayGoodbay(who) {
+   console.log('Good bye', who);   
+   if ( count-- <= 0 )
+      clearInterval(repeat);
+}
+
+var repeat = setInterval(sayGoodbay, 1 * 1000, 'Friend');
