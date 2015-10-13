31473be6941366048f2d079a5f977a202ffd451b
wannabewize <wannabewize@gmail.com>
2015-10-14 00:48:18 +0900

    readline module.

---
+++ BasicModules/readline.js
@@ -0,0 +1,16 @@
+// 모듈 로딩
+var readline = require('readline');
+
+// 객체 생성 - 생성 옵션
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// 객체에서 메소드 사용
+readline.cursorTo(process.stdout, 60, 30);
+
+// 이벤트 다루기
+rl.on('line', function(cmd) {
+   console.log('New line event ');
+});
