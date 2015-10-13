d1bc5e400685c81fb4353045ef2885cb47b81c6a
wannabewize <wannabewize@gmail.com>
2015-10-14 01:30:55 +0900

    이미지 없으면 애플리케이션 종료

--- HTTP/staticFileService1.js
+++ HTTP/staticFileService1.js
@@ -1,9 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
+// 이미지 파일 경로
+var path = './images/cat.jpg';
+try {
+   fs.accesssSync(path, fs.F_OK);
+}
+catch ( err ) {
+   console.log('/images/cat.jpg 파일 없음.');
+   process.exit(1);
+}
+
 var server = http.createServer(function(req, res) {
-	// 이미지 파일 경로
-	var path = './images/cat.jpg';
 	
 	fs.readFile(path, function(err, data) {
       res.statusCode = 200;
