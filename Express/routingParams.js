4de4f4fa9f98af8c279ff543abcb162495fe2d7d
wannabewize <wannabewize@gmail.com>
2015-10-16 02:16:21 +0900

    routing param sample

---
+++ Express/routingParams.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.listen(3000);
+
+// 파라미터 1개
+app.get('/user/:id', function (req, res) {
+   var userId = req.params.id;
+   res.send('User ID ' + userId);
+});
+
+// 파라미터 2개
+app.get('/movies/:movieId/:actor', function (req, res) {
+   var movieId = req.params.movieId;
+   var actor = req.params.actor;
+
+   res.send('Actor : ' + actor + ' -  Movie ID : ' + movieId);
+});
