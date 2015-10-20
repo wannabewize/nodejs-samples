e1bcad5aa65937d707dbd49b3ae9b7c7c2f47e59
wannabewize <wannabewize@gmail.com>
2015-10-21 00:29:05 +0900

    Echo Service with Express, socket.io

--- Socket.IO/echo_server.js
+++ Socket.IO/echo_server.js
@@ -1,15 +1,18 @@
+var express = require('express');
 var http = require('http');
 var fs = require('fs');
 
-var app = http.createServer(function(req, res) {
-	fs.createReadStream('./echo_client.html').pipe(res);
+var app = express();
+var server = http.createServer(app);
+server.listen(3000);
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/echo_client.html');
 });
 
-app.listen(8080);
-
-var io = require('socket.io')(app);
+var io = require('socket.io')(server);
 io.on('connection', function(socket){
-	console.log('Connection!');
+	console.log('클라이언트 접속');  
 	 
 	socket.on('message', function(data) {
 		console.log('message from client : ', data['message']);
