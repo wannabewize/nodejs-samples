6b61ed3bb40ba0133f75c3e9b795c97dbc394a4b
wannabewize <wannabewize@gmail.com>
2015-10-21 00:05:28 +0900

    socket.io 서버와 클라이언트 접속, 이벤트 샘플

---
+++ Socket.IO/socketio_server.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+	fs.createReadStream('./socketio_client.html').pipe(res);
+});
+
+server.listen(3000);
+
+var io = require('socket.io')(server);
+io.on('connection', function(socket){
+	console.log('클라이언트 접속');  
+	 	
+	socket.on('disconnect', function() {
+		console.log('Disconnected');
+	});
+});
