dac39f24f185baf5a9a3218c0abb6ea5ab49aa19
Lee Jaehoon <wannabewize@gmail.com>
2015-10-23 15:50:24 +0900

    thumbnail sample

---
+++ AWS/thumbnail.js
@@ -0,0 +1,14 @@
+var easyimg = require('easyimage');
+
+var imageFile = './image.jpg';
+var thumbnail = './image_thumb.jpg';
+
+easyimg.thumbnail({
+   src:imageFile,
+   dst:thumbnail,
+   width:100
+}).then(function(image){
+   console.log('thumbnail created : ', image);
+}, function(err) {						
+   console.error('Thumbanil Create Error', err);
+});
