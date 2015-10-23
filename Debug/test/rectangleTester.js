3303d2cfd93773c56e7ab7687b678fc608f5ef39
Lee Jaehoon <wannabewize@gmail.com>
2015-10-23 09:17:22 +0900

    test code in test folder

--- Debug/test/rectangleTester.js
+++ Debug/test/rectangleTester.js
@@ -1,19 +1,18 @@
 var assert = require('assert');
 var should = require('should');
-var Rectangle = require('./rectangle');
+var Rectangle = require('../rectangle');
 
 describe('Rectangle', function() {
    var r1 = new Rectangle(10, 20);
    var r2 = new Rectangle(20, 20);
    
-   it('isSquare', function(done) {
+   it('isSquare', function() {
       assert.equal(r1.isSquare, false, 'Rectangle(10,20)');
       assert.equal(r2.isSquare, true, 'Rectangle(20,20)');
-      done();
    });
-   it('size', function(done) {
+   
+   it('size', function() {
       r1.size().should.equal(10*20, 'Rectangle(10,20)');
       r2.size().should.equal(20*20, 'Rectangle(20,20)');
-      done();
    });
 });
