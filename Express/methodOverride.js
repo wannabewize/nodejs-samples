17768942c3732314828cda0d959fc15806dce009
wannabewize <wannabewize@gmail.com>
2015-10-26 01:25:42 +0900

    작성중

---
+++ Express/methodOverride.js
@@ -0,0 +1,31 @@
+// 작성중
+var express = require('express');
+var methodOverride = require('method-override');
+
+var app = express();
+app.use(methodOverride('_method'));
+
+app.get('/', showForm);
+
+app.post('/', function(req, res) {
+   res.send('Post Request');
+});
+
+app.delete('/resource', function(req, res) {
+   res.send('Delete Request');
+});
+
+app.listen(3000);
+
+function showForm(req, res) {
+   var body = '<html>';
+   body += '<head><meta charset="UTF-8"></head>';
+   body += '<body>';
+   body += '<h3>Method override Sample</h3>';
+   body += '<form method="POST" action="/" >';
+   body += '<input type="hidden" name="_method" value="DELETE">';
+   body += '<button type="submit">Delete</button>';
+   body += '</form>';
+   body += '</body></html>';
+   res.send(body);
+}
