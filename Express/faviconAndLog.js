cb568c7d0aa90de8730f331001bace74c2cb5150
wannabewize <wannabewize@gmail.com>
2015-10-26 01:08:56 +0900

    이름 변경

--- Express/faviconAndLog.js
+++ Express/faviconAndLog.js
@@ -3,19 +3,21 @@ var favicon = require('serve-favicon');
 var morgan = require('morgan');
 
 var app = express();
-app.use(favicon(__dirname + '/public/favicon111.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(morgan('dev'));
 
 app.use(handler);
 
-app.listen(3005, function(err) {
-   console.log('Server is listening @3005');
+// Random Response
+app.listen(3000, function(err) {
+   console.log('Server is listening @3000');
 });
 
 function handler(req, res) {
    // 정수형
-   var r = Math.round(Math.random() % 4);
+   var r = Math.round(Math.random() * 4);
    console.log(r);
+   r = 0;
    switch (r) {
       case 0:
          // Not finish
