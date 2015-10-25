0ac812cac2b308444f7a5067665ef63758204088
Lee Jaehoon <wannabewize@gmail.com>
2015-10-25 21:45:30 +0900

    경로 모듈 샘플 업데이트

--- BasicModules/path.js
+++ BasicModules/path.js
@@ -10,15 +10,23 @@ console.log('normalize : ', normalizePath);
 console.log('dirname : ', pathUtil.dirname(normalizePath));
 console.log('basename : ', pathUtil.basename(normalizePath, 'html'));
 
+var pathStr = '/foo/bar/baz/asdf/quux.html';
+console.log('== Path Str : ', pathStr);
+console.log('dirname : ', pathUtil.dirname(pathStr) );
+console.log( 'basename : ',  pathUtil.basename(pathStr) ); // ‘quux.html'
+console.log( 'extname : ', pathUtil.extname(pathStr) );
+
 // Parse
 var parsed = pathUtil.parse(__filename);
 console.log('parsed : ', parsed);
 console.log('parsed.ext : ', parsed.ext);
 
-
 // 경로 덧붙이기
 var joined = pathUtil.join('/tp/login/', 'game');
 console.log('join : ', joined);
+
+var pathStr2 = __dirname + pathUtil.sep + 'image.png';
+console.log('Path making : ',pathStr2);
 
 // 경로 만들기
 var path = pathUtil.format({
