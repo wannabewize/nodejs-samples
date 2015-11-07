79f326c68f814f99c7d26792c38582acac334c6f
wannabewize <wannabewize@gmail.com>
2015-11-07 13:07:41 +0900

    os 모듈

---
+++ BasicModules/os.js
@@ -0,0 +1,22 @@
+var os = require('os');
+
+console.log('tmpdir : ', os.tmpdir());
+console.log('homedir : ', os.homedir());
+// 'BE' for big endian or 'LE' for little endian.
+console.log('endian : ', os.endianness());
+console.log('hostname : ', os.hostname());
+
+console.log('type : ', os.type());
+console.log('platform : ', os.platform());
+console.log('arch : ', os.arch());
+console.log('release : ', os.release());
+
+console.log('uptime : ', os.uptime());
+console.log('loadavg : ', os.loadavg());
+
+console.log('totlamem : ', os.totalmem());
+console.log('freemem : ', os.freemem());
+
+console.log('cpus : ', os.cpus());
+console.log('network interfaces : ', os.networkInterfaces());
+console.log('eol : ', os.EOL);
