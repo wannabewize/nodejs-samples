d0e78163200368f15944133f713106b3e08dd92a
wannabewize <wannabewize@gmail.com>
2015-11-14 15:35:08 +0900

    crypto sample. ing

--- BasicModules/crypto.js
+++ BasicModules/crypto.js
@@ -6,4 +6,15 @@ console.log(ciphers); // ['aes-128-cbc', 'aes-128-ccm', ...]
 
 
 console.log('== Hash');
-console.log(crypto.getHashes());
+// console.log(crypto.getHashes());
+var hash1 = crypto.createHash('md5');
+
+hash1.update('Hello');
+var hashed = hash1.digest('hex');
+console.log('hashed : ', hashed);
+
+var hash2 = crypto.createHmac('sha1', 'secret key');
+hash2.update('Hello');
+var hashed2 = hash2.digest('hex');
+console.log('hashed2 : ', hash2);
+
