56bab69c87c786082a998fda88c6e9c2e1b1d836
wannabewize <wannabewize@gmail.com>
2015-11-15 20:51:20 +0900

    console module sample ing

---
+++ BasicModules/console.js
@@ -0,0 +1,13 @@
+
+console.log('console.log', 'log message');
+console.info('console.info', 'info message');
+console.warn('console.warn', 'warn message');
+console.error('console.error', 'error message');
+
+
+
+var obj = {
+   name : 'iu'
+}
+
+console.dir(obj);
