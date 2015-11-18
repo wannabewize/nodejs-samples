5b9d499e0fb0f8d42efd2dfd392ae620dc7f1bce
Lee Jaehoon <wannabewize@gmail.com>
2015-11-18 11:42:02 +0900

    주석 추가

--- Auth/LocalAuth_Step2/server.js
+++ Auth/LocalAuth_Step2/server.js
@@ -14,14 +14,14 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 // 바디파서 - 필수
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // 세션 모듈 설정 - 필수
 var session = require('express-session');
 app.use(session({
-  secret: 'Secret Key',
-  resave: false,
-  saveUninitialized: true
+   secret: 'Secret Key',
+   resave: false,
+   saveUninitialized: true
 }));
 
 var passport = require('passport');
@@ -30,49 +30,50 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 var defaultUser = {
-   id : 'user',
-   password : '1234',
-   name : '사용자'
+   id: 'user',
+   password: '1234',
+   name: '사용자'
 }
 
 // Local Strategy
 var LocalStrategy = require('passport-local').Strategy;
 var strategy = new LocalStrategy({ passReqToCallback: true },
    function (req, username, password, done) {
-     if ( username == defaultUser.id && password == defaultUser.password ) {
-        console.log('로그인 성공');
-        return done(null, defaultUser);
-     }     
-     console.log('로그인 실패');
-     return done(null, false, {message:'로그인 실패'});
+      if (username == defaultUser.id && password == defaultUser.password) {
+         console.log('로그인 성공');
+         return done(null, defaultUser);
+      }
+      console.log('로그인 실패');
+      return done(null, false, { message: '로그인 실패' });
    }
-);
+   );
 passport.use(strategy);
 
 // 세션에 기록하기
-passport.serializeUser(function(user, done) {
-  console.log('serializeUser', user);  
-  done(null, user.id);
+passport.serializeUser(function (user, done) {
+   console.log('serializeUser', user);
+   done(null, user.id);
 });
 
 // 세션에서 사용자 정보 얻어오기
-passport.deserializeUser(function(id, done) {
-  console.log('deserializeUser', id);
-  done(null, defaultUser);
+passport.deserializeUser(function (id, done) {
+   //serializeUser에서 user.id로 반환했으므로 user.id가 온다. 객체로 설정 가능
+   console.log('deserializeUser', id);
+   done(null, defaultUser);
 });
 
 // 로그인 페이지
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
    console.log('authorized : ', req.isAuthenticated());
-   res.render('login', {isAuthorized:req.isAuthenticated()} ) 
+   res.render('login', { isAuthorized: req.isAuthenticated() })
 });
 
 // 로그인 요청
-app.post('/login', passport.authenticate('local', { successRedirect: '/myHome', failureRedirect: '/login'}));
+app.post('/login', passport.authenticate('local', { successRedirect: '/myHome', failureRedirect: '/login' }));
 
-app.get('/myHome', isAuthenticated, function(req, res) {
+app.get('/myHome', isAuthenticated, function (req, res) {
    // 인증된 상태이므로 myHome 템플릿 페이지 렌더링
-   res.render('myHome', {user:req.user});
+   res.render('myHome', { user: req.user });
 });
 
 function isAuthenticated(req, res, next) {
@@ -86,13 +87,13 @@ function isAuthenticated(req, res, next) {
 
 
 // 로그아웃
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
    console.log('로그아웃');
    req.logout();
    res.redirect('/login');
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
    res.redirect('/login');
 })
 
