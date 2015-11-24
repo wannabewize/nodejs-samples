11ece4b6a44105a7e0bee9b09b1821ccee19a4e1
Lee Jaehoon <wannabewize@gmail.com>
2015-11-25 01:57:23 +0900

    상속. 간단한 예제 추가

--- BasicModules/inherits.js
+++ BasicModules/inherits.js
@@ -2,6 +2,24 @@ console.log('== inherits ==');
 
 var util = require('util');
 
+
+function Parent() {
+}
+Parent.prototype.sayHello = function() {
+   console.log('Hello. from Parent Class');
+}
+
+function Child() {
+   
+}
+
+util.inherits(Child, Parent);
+
+var child = new Child();
+child.sayHello();
+
+
+
 function Rectangle(width, height) {
    this.width = width;
    this.height = height;
