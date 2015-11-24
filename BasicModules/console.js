e5bbd77d8caad0133c7cedde798b990fc9666043
wannabewize <wannabewize@gmail.com>
2015-11-25 02:39:35 +0900

    콘솔, 프로세스 예제

--- BasicModules/console.js
+++ BasicModules/console.js
@@ -1,8 +1,31 @@
+// Basic Log
+var intValue = 3;
+console.log('int Value ' + 3);
+
+var obj = {
+   name : 'IU',
+   job : 'Singer'
+}
+
+console.log('obj : ' + obj);
+//obj : [object Object]
+
+console.log('obj : ', obj);
+// obj :  { name: 'IU', job: 'Singer' }
+
+var array = [1, 2, 3];
+console.log('array : ' + array);
+console.log('array : ' , array);
+
+// Error Stack Info
+var error = new Error('Error');
+console.log('Error : ', error.stack);
+
 // Log Level
-console.log('console.log', 'log message');
-console.info('console.info', 'info message');
-console.warn('console.warn', 'warn message');
-console.error('console.error', 'error message');
+console.log('log', 'log message');
+console.info('info', 'info message');
+console.warn('warn', 'warn message');
+console.error('error', 'error message');
 
 var fs = require('fs');
 var Console = require('console').Console;
@@ -12,7 +35,7 @@ var errorOutput = fs.createWriteStream('./stderr.log');
 var logger = new Console(output, errorOutput);
 
 // stdout.log 파일로 로그 출력
-logger.log('info','정보 메세지');
+logger.info('info','정보 메세지');
 logger.log('log','로그 메세지');
 
 // stderr.log 파일로 경과, 에러 메세지 출력
--- BasicModules/process.js
+++ BasicModules/process.js
@@ -1,17 +1,8 @@
-console.log('argv 개수 : ', process.argv.length);
-
-console.log('0번째 : ', process.argv[0]); // node 명령어
-console.log('1번째 : ', process.argv[1]); // js 파일
-
-// 파라미터로 입력한 값의 합 구하기
-var sum = 0;
-for( var i = 2 ; i < process.argv.length ; i++ ) {
-   var arg = process.argv[i];
-   var value = parseInt(arg);
-   // 숫자만 더하기
-   if ( value != NaN ) {
-      sum += value;
-   }
-}
-
-console.log('Sum : ' + sum);
+console.log('== 프로세스 정보 ==');
+console.log('- process.env: ', process.env);
+console.log('- process.version: ', process.version);
+console.log('- process.versions: ', process.versions);
+console.log('- process.arch:', process.arch);
+console.log('- process.platform: ', process.platform);
+console.log('- process.memoryUsage():', process.memoryUsage());
+console.log('- process.uptime():', process.uptime());
