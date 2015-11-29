dab6e1c58a6c7b7ba4e31997f2564a0e90de3722
Lee Jaehoon <wannabewize@gmail.com>
2015-11-29 19:24:45 +0900

    비동기 예제

--- BasicModules/fs/fs_write2.js
+++ BasicModules/fs/fs_write2.js
@@ -1,6 +1,13 @@
 var fs = require('fs');
 
-fs.writeFileSync('./textData.txt', 'Hello World');
+// 기본 인코딩 : utf-8
+fs.writeFile('./textData.txt', 'Hello World', 'utf-8', function(err) {
+   if ( err ) {
+      console.error('File write Errro : ', err);
+      return;
+   }
+   console.log('파일 저장 성공');
+});
 
 // 버퍼로 저장
 var buffer = new Buffer([10,20,30,40,50]);
@@ -11,19 +18,3 @@ fs.writeFile('./binaryData.dat', buffer, function(err) {
    }
    console.log('파일 저장 성공');      
 });
-
-// 파일에 내용 추가
-fs.appendFile('./textData.txt', '\nGood morning', function(err) {
-   if ( err ) {
-      console.error('파일 내용 추가 실패 : ', err);
-      return;
-   }
-   console.log('파일 내용 추가 성공');      
-});
-
-// 파일 삭제
-fs.unlink('./binaryData.dat', function(err) {
-   if ( err ) {
-      console.error('Delete Error : ', err);
-   }
-});   
