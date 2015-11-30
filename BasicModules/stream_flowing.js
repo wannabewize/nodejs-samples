6742930adc85b9227b9a91c8d49795a5489726a0
Lee Jaehoon <wannabewize@gmail.com>
2015-11-30 23:15:12 +0900

    스트림 예제. flowing, paused mode

--- BasicModules/stream_flowing.js
+++ BasicModules/stream_flowing.js
@@ -1,16 +1,15 @@
 var fs = require('fs');
 
-var file = 'stream_pause.js';
+var file = 'stream_flowing.js';//'image.jpg';
 var is = fs.createReadStream(file);
 
-// 'data' 이벤트가 없으면 paused mode
 is.on('readable', function() {
    console.log('== READABLE EVENT');   
-   
-   // 10바이트씩 읽기
-   while( chunk = is.read(10) ) {
-      console.log('chunk : ', chunk.toString());  
-   }
+});
+
+is.on('data', function(chunk) {
+   console.log('== DATA EVENT');
+   console.log(chunk.toString());   
 });
 
 // end 이벤트
