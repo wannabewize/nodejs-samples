ffb0d8438f80ad45a3a1b98e29aee62844eb4893
wannabewize <wannabewize@gmail.com>
2015-12-08 02:30:09 +0900

    흐름 제어 예제 모으기

---
+++ BasicModules/FlowControl/async_parallel.js
@@ -0,0 +1,38 @@
+var async = require('async');
+
+async.parallel(
+   [
+      function(callback) {
+         console.log('태스크 1 시작');
+         setTimeout(function() {
+            console.log('태스크 1 종료');  
+            callback(null, '태스크 1 결과');          
+         }, 1000);
+      },
+      function(callback) {
+         console.log('태스크 2 시작');
+         setTimeout(function() {
+            console.log('태스크 2 종료');            
+            callback(null, '태스크 2 결과');
+         }, 2000);
+      },
+      task3      
+   ],
+   function(err, results) {
+      if ( err ) {
+         console.error('에러 : ', err);
+         return;
+      }
+      
+      console.log('모든 태스크 종료, 결과 : ', results);
+   }
+);
+
+
+function task3(callback) {
+   console.log('태스크 3 시작');
+   setTimeout(function() {
+      console.log('태스크 3 종료');      
+      callback(null, '태스크 3 결과');
+   }, 2000);         
+}
