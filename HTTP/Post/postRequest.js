ba00ba4fb083b7b8d37a8eb48c518124f4db1d51
wannabewize <wannabewize@gmail.com>
2015-12-15 01:45:30 +0900

    POST 요청 예제

---
+++ HTTP/Post/postRequest.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var query = require('querystring');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+   if (req.method.toLowerCase() == 'post') {
+      handlePostReqeust(req, res);
+   }
+   // get이면 폼 목록 출력
+   else {      
+      fs.createReadStream('form.html').pipe(res);      
+   }
+});
+
+function handlePostReqeust(req, res) {
+   var buffer = '';
+   req.on('data', function (chunk) {
+      console.log('data event : ' + chunk.length);
+      buffer += chunk;
+   });
+   req.on('end', function () {
+      console.log('end : ' + buffer);
+			
+      // 바디 파싱
+      var parsed = query.parse(buffer);      
+      // JSON 형태로 출력      
+      res.end(JSON.stringify(parsed));
+      
+      
+   });
+}
+
+server.listen(3000);
+console.log('Server is running on 3000');
