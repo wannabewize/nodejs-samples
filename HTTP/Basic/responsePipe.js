a77f152376fd6f1ae0d6f47280d7b1646c51a1e1
wannabewize <wannabewize@gmail.com>
2015-12-15 01:45:14 +0900

    기본 모듈

---
+++ HTTP/Basic/responsePipe.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+var path = 'movie/movie.mp4'
+
+http.createServer(function(req, res) {   
+   res.setHeader('content-type','video/mp4');   
+   fs.readFile(path, function(err, data) {
+      res.end(data);
+   });      
+}).listen(3000);
+
+http.createServer(function(req, res) {
+   res.setHeader('content-type','video/mp4');
+   fs.createReadStream(path).pipe(res);
+   
+}).listen(3001);
