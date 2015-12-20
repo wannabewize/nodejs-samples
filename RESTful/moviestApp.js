efeba175dee95d16a7bab6392bf17d9bfbe624fa
wannabewize <wannabewize@gmail.com>
2015-12-21 04:50:16 +0900

    RESTful 서비스

--- RESTful/moviestApp.js
+++ RESTful/moviestApp.js
@@ -4,67 +4,79 @@ var fs = require('fs');
 var data = fs.readFileSync('./movieData.json');
 var movieList = JSON.parse(data);
 
-var server = http.createServer(function(req, res) {
-	var method = req.method.toLowerCase();	
-	switch (method) {
-		case 'get':
-		handleGetRequest(req, res);
-		return;		
-		case 'post':
-		handlePostRequest(req, res);
-		return;
-		case 'put':
-		handlePutRequest(req, res);
-		return;
-		case 'delete':		
-		handleDeleteRequest(req, res);
-		return;
-		default:
-		res.statusCode = 404;
-		res.end('Wrong method');
-		return;		
-	}
+var server = http.createServer(function (req, res) {
+   var method = req.method.toLowerCase();
+   switch (method) {
+      case 'get':
+         handleGetRequest(req, res);
+         return;
+      case 'post':
+         handlePostRequest(req, res);
+         return;
+      case 'put':
+         handlePutRequest(req, res);
+         return;
+      case 'delete':
+         handleDeleteRequest(req, res);
+         return;
+      default:
+         res.statusCode = 404;
+         res.end('Wrong method');
+         return;
+   }
 });
 server.listen(3000);
 
 function handleGetRequest(req, res) {
    var url = req.url;
    var list = [];
-   movieList.forEach(function(item) {
-      list.push( {movieId : item.movieId, title:item.title} );
+   movieList.forEach(function (item) {
+      list.push({ movieId: item.movieId, title: item.title });
    });
-   if ( url == '/movies' ) {
-      res.writeHead(200, {'Content-Type':'application/json'});		     
-      res.end(JSON.stringify(list));
+   if (url == '/movies') {
+      var list = [];
+      for (var i = 0; i < movieList.length; i++) {
+         var movie = movieList[i];
+         list.push({ id: movie.id, title: movie.title });
+      }
+
+      var result = {
+         count: list.length,
+         data: list
+      }
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(result));
    }
    else {
-      var movieId = url.split('/')[2];
+      var id = url.split('/')[2];
       var movie = null;
-      for(var i = 0 ; i < movieList.length; i++) {
+      for (var i = 0; i < movieList.length; i++) {
          var item = movieList[i];
-         if ( movieId == item.movieId) {
+         if (id == item.id) {
             movie = item;
             break;
          }
-      }      
-      if ( movie ) {
+      }
+      if (movie) {
+         res.writeHead(200, { 'Content-Type': 'application/json' });
          res.end(JSON.stringify(movie));
       }
       else {
-         res.status(404);
-         res.end('Can not find movie info');         
+         res.writeHead(404, { 'Content-Type': 'application/json' });
+         res.end(JSON.stringify({error : 'Can not find movie info'}));
       }
    }
 }
 
 function handlePostRequest(req, res) {
-   
+
 }
 
 function handlePutRequest(req, res) {
-   
+
 }
 
 function handleDeleteRequest(req, res) {
-   
+
 }
