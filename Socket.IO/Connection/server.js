ac5f832a10d84de096e3e3620f4859f72cd54e4d
wannabewize <wannabewize@gmail.com>
2015-12-24 03:16:12 +0900

    Socket.IO 예제 구조 변경

--- Socket.IO/Connection/server.js
+++ Socket.IO/Connection/server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (req, res) {
-   fs.createReadStream('./socketio_client.html').pipe(res);
+   fs.createReadStream('./client.html').pipe(res);
 });
 
 server.listen(3000);
