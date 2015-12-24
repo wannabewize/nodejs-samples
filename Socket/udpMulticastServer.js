1df83e36f40b826c28060897d3d089eabcd19c33
wannabewize <wannabewize@gmail.com>
2015-12-24 15:45:53 +0900

    멀티캐스트 예제

--- Socket/udpMulticastServer.js
+++ Socket/udpMulticastServer.js
@@ -2,19 +2,6 @@ var dgram = require('dgram');
 
 var socket = dgram.createSocket('udp4');
 
-socket.on('message', function(msg, rinfo) {
-	console.log('Message Event\n', 'Message : ', msg.toString(), ' from : ', rinfo.address, rinfo.port);
-	// socket.send(msg, 0, msg.length, 3000, rinfo.address);
-});
-
-socket.on('listening', function() {
-	console.log('listening Event');
-});
-
-socket.on('close', function() {
-	console.log('Socket closed');
-});
-
 // 서버에서 작성한 내용을 클라이언트로 멀티 캐스트
 var is = process.stdin;
 
@@ -23,7 +10,7 @@ is.on('data', function(chunk) {
    if ( msg.trim() == 'exit' ) {
       // 종료
       is.end();
-      socket.close();                  
+      socket.close();
    }
    else {      
       socket.send(msg, 0, msg.length, 3000, '224.0.0.114', function(err) {
