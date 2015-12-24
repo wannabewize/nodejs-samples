b86b4e6df20ac9fd8a8527893a76289c1c41b581
wannabewize <wannabewize@gmail.com>
2015-12-24 15:36:20 +0900

    멀티캐스트 예제

--- Socket/udpMulticastClient.js
+++ Socket/udpMulticastClient.js
@@ -1,30 +1,15 @@
 var dgram = require('dgram');
 var socket = dgram.createSocket('udp4');
 
-var is = process.stdin;
+socket.bind(3000);
 
-is.on('data', function(chunk) {
-   var msg = chunk.toString();
-   if ( msg.trim() == 'exit' ) {
-      // 종료
-      is.end();
-      socket.close();                  
-   }
-   else {      
-      socket.send(msg, 0, msg.length, 3000, '224.0.0.114', function(err) {
-         if ( err ) {
-            console.error('UDP Message send error.', err);   
-            return;
-         }         
-         console.log('UDP Message Send success');
-      });      
-   }
+socket.on('listening', function() {
+   socket.addMembership('224.0.0.114');   
 });
 
 socket.on('message', function(msg, rinfo) {
    console.log(rinfo.address,' >> ', msg.toString());   
 });
 
-socket.bind(3000);
 
 
--- Socket/udpMulticastServer.js
+++ Socket/udpMulticastServer.js
@@ -1,9 +1,6 @@
 var dgram = require('dgram');
 
 var socket = dgram.createSocket('udp4');
-socket.bind(function() {
-   socket.addMembership('224.0.0.114');
-});
 
 socket.on('message', function(msg, rinfo) {
 	console.log('Message Event\n', 'Message : ', msg.toString(), ' from : ', rinfo.address, rinfo.port);
@@ -16,4 +13,25 @@ socket.on('listening', function() {
 
 socket.on('close', function() {
 	console.log('Socket closed');
+});
+
+// 서버에서 작성한 내용을 클라이언트로 멀티 캐스트
+var is = process.stdin;
+
+is.on('data', function(chunk) {
+   var msg = chunk.toString();
+   if ( msg.trim() == 'exit' ) {
+      // 종료
+      is.end();
+      socket.close();                  
+   }
+   else {      
+      socket.send(msg, 0, msg.length, 3000, '224.0.0.114', function(err) {
+         if ( err ) {
+            console.error('UDP Message send error.', err);   
+            return;
+         }         
+         console.log('UDP Message Send success');
+      });      
+   }
 });
