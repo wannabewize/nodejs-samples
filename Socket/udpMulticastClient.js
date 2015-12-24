e895ca6e9ceaecf65567efe294fced1080855ddd
wannabewize <wannabewize@gmail.com>
2015-12-24 15:29:00 +0900

    udp 멀티캐스트 예제

---
+++ Socket/udpMulticastClient.js
@@ -0,0 +1,28 @@
+var dgram = require('dgram');
+var socket = dgram.createSocket('udp4');
+
+var is = process.stdin;
+
+is.on('data', function(chunk) {
+   var msg = chunk.toString();
+   if ( msg.trim() == 'exit' ) {
+      // 종료
+      is.end();
+      socket.close();                  
+   }
+   else {      
+      socket.send(msg, 0, msg.length, 3000, '224.0.0.114', function(err) {
+         if ( err ) {
+            console.error('UDP Message send error.', err);   
+            return;
+         }         
+         console.log('UDP Message Send success');
+      });      
+   }
+});
+
+socket.on('message', function(msg, rinfo) {
+   console.log(rinfo.address,' >> ', msg.toString());   
+});
+
+
