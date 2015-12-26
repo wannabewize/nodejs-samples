e760ab4af91856b1425db661f8bd2ac57e289e81
wannabewize <wannabewize@gmail.com>
2015-12-26 21:48:21 +0900

    소켓io 예제

--- Socket.IO/Connection/server.js
+++ Socket.IO/Connection/server.js
@@ -1,11 +1,21 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function (req, res) {
-   fs.createReadStream('./client.html').pipe(res);
+var server = http.createServer(app);
+server.listen(3000);
+
+app.get('/', function(req, res) {
+   res.sendFile(__dirname + '/client.html');
 });
 
-server.listen(3000);
+// HTTP 모듈
+// var server = http.createServer(function (req, res) {
+//    fs.createReadStream('./client.html').pipe(res);
+// });
+
+// server.listen(3000);
 
 // Socket.IO Server
 // var IOServer = require('socket.io');
@@ -16,6 +26,7 @@ var io = require('socket.io')(server);
 io.on('connection', function (socket) {
    console.log('클라이언트 접속');
 
+   // 원격 호스트 접속 종료 이벤트
    socket.on('disconnect', function () {
       console.log('Disconnected');
    });
