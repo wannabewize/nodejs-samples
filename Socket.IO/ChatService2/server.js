56bab32b08170cb2566ea857a5b9ecf078098f68
Lee Jaehoon <wannabewize@gmail.com>
2015-12-27 15:34:51 +0900

    Socket.iO 예제

--- Socket.IO/ChatService2/server.js
+++ Socket.IO/ChatService2/server.js
@@ -15,25 +15,25 @@ app.get('/', function(req, res) {
 });
 
 // 닉네임 기능
-var nickNames = {};
+var users = {};
 
 var io = require('socket.io')(server);
 io.on('connection', function(socket){
 
    // 닉네임 등록. socket.id로 구분     
    var nickName = 'Guest' + Math.floor(Math.random()*100);
-   nickNames[socket.id] = nickName; 
+   users[socket.id] = nickName; 
    console.log(nickName + ' connected');
    
 	// 개별 클라이언트에 환영 메세지
-	socket.emit('serverMessage', {nick:'Admin', msg:'Welcome to Socket.IO Chat-Service, ' + nickName});
+	socket.emit('chatMessage', {nick:'Admin', msg:'Welcome to Socket.IO Chat-Service, ' + nickName});
 	 
 	 // 채팅 메세지는 모든 클라이언트에게
-	socket.on('clientMessage', function(data) {
+	socket.on('chatInput', function(data) {
       var msg = data['message'];
-      var sender = nickNames[socket.id];      
+      var sender = users[socket.id];      
 		console.log(sender + ' >> '+ msg);		
-      io.emit('serverMessage', {nick:sender, msg:msg});
+      io.emit('chatMessage', {nick:sender, msg:msg});
 	});
    	
 	socket.on('disconnect', function() {
--- Socket.IO/ChatService3/server.js
+++ Socket.IO/ChatService3/server.js
@@ -1,3 +1,7 @@
+/**
+ * ChatService with Socket.IO
+ * Room example
+ */
 var express = require('express');
 var http = require('http');
 
@@ -5,33 +9,46 @@ var app = express();
 var server = http.createServer(app);
 server.listen(3000);
 
-var rooms = ['server', 'client', 'designer', 'project'];
+// 채팅방
+var rooms = ['trevel', 'game', 'dining', 'All'];
 
+// 채팅방 목록
 app.get('/rooms', function(req, res) {
    res.json(rooms);
 });
 
 app.get('/', function(req, res) {
-   res.sendFile(__dirname + '/chat_client.html');
+   res.sendFile(__dirname + '/client.html');
 });
+
+// 닉네임 기능
+var users = {};
 
 var io = require('socket.io')(server);
 io.on('connection', function(socket){
 	console.log('client Connected!');
    
+   // 닉네임 등록. socket.id로 구분     
+   var nickName = 'Guest' + Math.floor(Math.random()*100);
+   users[socket.id] = nickName; 
+   console.log(nickName + ' connected');
+   
    var room;
 	
 	// 개별 클라이언트에 환영 메세지
-	socket.emit('message', {message:'Welcome to Socket.IO Chat Service'});
+	socket.emit('chatMessage', {nick:'Admin', msg:'Welcome to Socket.IO Chat Service'});
 	 
 	 // 채팅 메세지는 모든 클라이언트에게
-	socket.on('clientMessage', function(data) {
+	socket.on('chatInput', function(data) {
+      var msg = data['message'];
+      var sender = users[socket.id];      
+      
 		console.log('message from client : ', data['message']);		
       
       if ( room )
-         io.to(room).emit('serverMessage', data);
+         io.to(room).emit('chatMessage', data);
       else
-         io.emit('serverMessage', data);
+         io.emit('chatMessage', data);
 	});
    
    // 채팅방 들어가기
