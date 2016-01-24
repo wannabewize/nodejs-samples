30657411e1285815e4260bac7820ef7cf3d5d33e
wannabewize <wannabewize@gmail.com>
2016-01-25 02:16:38 +0900

    util 모듈의 상속 예제, instanceof 출력.

--- BasicModules/inherits.js
+++ BasicModules/inherits.js
@@ -17,26 +17,6 @@ util.inherits(Child, Parent);
 
 var child = new Child();
 child.sayHello();
+console.log('instanceof Child : ',child instanceof Child);
+console.log('instanceof Parent : ',child instanceof Parent);
 
-
-
-function Rectangle(width, height) {
-   this.width = width;
-   this.height = height;
-}
-
-Rectangle.prototype.size = function() {
-   return this.width * this.height;
-}
-
-function Square(length) {
-   this.width = length;
-   this.height = length;   
-}
-// prototype을 이용한 상속 방법
-// Square.prototype = Rectangle.prototype;
-// util.inherits를 이용한 상송
-util.inherits(Square, Rectangle);
-
-var r = new Square(10);
-console.log(r.size());
