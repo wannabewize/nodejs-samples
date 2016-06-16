0db3f66ff6a210dbb2a9619dd68b7797def93b95
wannabewize <wannabewize+macbookpro@gmail.com>
2016-06-16 22:44:52 +0900

    angular 예제

--- Angular/Medium/Router/routerExample.js
+++ Angular/Medium/Router/routerExample.js
@@ -1,14 +1,16 @@
-var app = angular.module('ExampleApp', []);
+var app = angular.module('ExampleApp', ['ngRoute']);
 
-app.config(['$routeProvider', function config$routeProvider) {
+app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/page1', {templateUrl:'page1.html', controller:PageController1})
-        .when('/page2', {templateUrl:'page2.html', controller:PageController2});
+        .when('/page2', {templateUrl:'page2.html', controller:PageController2})
+        .otherwise({redirectTo:'/page1'});
 }]);
 
 function PageController1($scope) {
     $scope.title = 'Page1';
-})
+};
 function PageController2($scope) {
     $scope.title = 'Page2';
-});
+    $scope.message = 'Welcome To Second Page';
+};
