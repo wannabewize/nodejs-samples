8e0feb199604169186f2ab76d2769a4d6d852d8e
wannabewize <wannabewize+macbookpro@gmail.com>
2016-06-16 16:37:37 +0900

    Angular 예제

---
+++ Angular/TodoExample/todo.js
@@ -0,0 +1,32 @@
+var app = angular.module('TodoExample', []);
+
+
+app.controller('TodoCtrl', function($scope) {
+    $scope.todos = [{title:'todo1', done:false}]
+
+    $scope.remaining = function() {
+        var count = 0;
+        angular.forEach($scope.todos, function(item) {
+            if ( ! item.done ) {
+                count++;
+            }
+        });
+        return count;
+    };
+
+
+    $scope.addTodo = function() {
+        $scope.todos.push({title:$scope.newTodo, done:false});
+        $scope.newTodo = '';
+    };
+
+    $scope.archive = function() {
+        var todo = [];
+        angular.forEach($scope.todos, function(item) {
+            if ( ! item.done ) {
+                todo.push(item);
+            }
+        });
+        $scope.todos = todo;
+    };
+});
