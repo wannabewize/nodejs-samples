a7c63de80ae61e754db6eb404e8e1ee9594a0ef2
wannabewize <wannabewize@gmail.com>
2016-06-19 19:11:48 +0900

    Angular example

---
+++ Angular/ApiServer/server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+// Cross Origin Resource Sharing
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.get('/greeting', function(req, res) {
+	var obj = {
+		hello : 'Hello Angular'
+	}
+	res.json(obj);
+});
+
+app.post('/greeting', function(req, res) {
+	if ( req.body.hello && req.body.hello.length > 3) {
+		res.json({message:'success'});
+	}
+	else {
+		res.sendStatus(400);
+	}
+});
+
+app.listen(3000);
