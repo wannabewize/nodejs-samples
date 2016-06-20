4532c511469ab15f525a01020d87db55e2808882
wannabewize <wannabewize@gmail.com>
2016-06-21 02:16:15 +0900

    MEAN 예제

--- Angular/Example/Moviest/server.js
+++ Angular/Example/Moviest/server.js
@@ -5,6 +5,8 @@ var movieRouter = require('./routes/movie_router');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/public/index.html');
 });
