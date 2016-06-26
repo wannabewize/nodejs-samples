555857f37838364c5a227345a6d00e333ad77b85
wannabewize <wannabewize@gmail.com>
2016-06-27 00:58:08 +0900

    Angular ngResource 예제

--- Angular/Medium/REST/server.js
+++ Angular/Medium/REST/server.js
@@ -12,25 +12,25 @@ app.get('/', function(req, res) {
 
 app.route('/movies')
 	.get(function(req, res) {
-		res.send({msg:'Get Request', data : ['Starwars', 'Interstella', 'Gravity', 'Avata']});
+		res.send({msg:'Get /movies Request', data : ['Starwars', 'Interstella', 'Gravity', 'Avata']});
 	}).post(function(req, res) {
-		res.send({msg:'Post Request', data : {title : req.body.title, director : req.body.director}});
+		res.send({msg:'Post /movies Request', data : {title : req.body.title, director : req.body.director}});
 	}).put(function(req, res) {
-		res.send({msg:'Put Request'});
+		res.send({msg:'Put /movies Request'});
 	}).delete(function(req, res) {
-		res.send({msg:'Delete Request'});
+		res.send({msg:'Delete /movies Request'});
 	}
 );
 
 app.route('/movies/:item')
 	.get(function(req, res) {
-		res.send({msg:'Get Request', data : {title:'Avata', director:'James Cameron', synopsis:'투르크막토 짱'} })
+		res.send({msg:'Get MovieDetail Request', data : {title:'Avata', director:'James Cameron', synopsis:'투르크막토 짱'} })
 	}).post(function(req, res) {
-		res.send({msg:'Post Request', param:req.params.item});
+		res.send({msg:'Post MovieDetail Request', param:req.params.item});
 	}).put(function(req, res) {
-		res.send({msg:'Put Request', param:req.params.item});
+		res.send({msg:'Put MovieDetail Request', param:req.params.item});
 	}).delete(function(req, res) {
-		res.send({msg:'Delete Request', param:req.params.item});
+		res.send({msg:'Delete MovieDetail Request', param:req.params.item});
 	}
 );
 
