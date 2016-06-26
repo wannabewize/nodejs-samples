3bda1c3e707b8a50eeeaa0d38dfd303d455d5cd4
wannabewize <wannabewize+macbookpro@gmail.com>
2016-06-26 19:33:48 +0900

    Angualar 예제

--- Angular/Example/Moviest/server.js
+++ Angular/Example/Moviest/server.js
@@ -3,8 +3,10 @@ var bodyParser = require('body-parser');
 var movieRouter = require('./routes/movie_router');
 
 var app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+// 정적 파일 제공 서비스
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
