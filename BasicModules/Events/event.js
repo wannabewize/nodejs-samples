12db3de156e9287e9b9dd3a90e54d310e5a652a2
wannabewize <wannabewize@gmail.com>
2016-07-03 00:43:08 +0900

    arrow function

--- BasicModules/Events/event.js
+++ BasicModules/Events/event.js
@@ -1,13 +1,24 @@
 // On - 이벤트 리스너 등록
+// ES6. 
+process.on('exit', code => {
+   console.log('Exit event : ' + code); 
+});
+/*
 process.on('exit', function(code) {
    console.log('Exit event : ' + code);
 });
+*/
 
 
 // Once - 이벤트 리스너 등록
+process.once('exit', ()=> {
+   console.log('Exit 이벤트 최초 발생');
+});
+/*
 process.once('exit', function() {
    console.log('Exit 이벤트 최초 발생');
 });
+*/
 
 // 이벤트 강제 발생
 process.emit('exit');
@@ -18,9 +29,15 @@ process.emit('exit', 1);
 /**
  * Uncaughted Exception
  */
+// ES6
+process.on('uncaughtException', ()=>{
+   console.log('uncaughtException 발생!');  
+});
+/*
 process.on('uncaughtException', function() {
    console.log('uncaughtException 발생!');   
 });
+*/
 
 // 없는 함수 실행
 justDoIt();
