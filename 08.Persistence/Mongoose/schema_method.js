d50ac32c24313cedf962d2f5a390d10bf5be5182
wannabewize <wannabewize+macbookpro@gmail.com>
2016-12-07 22:19:31 +0900

    mongodb example

---
+++ 08.Persistence/Mongoose/schema_method.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+const url = 'mongodb://localhost:27017/samples';
+mongoose.connect(url);
+
+const ValueSchema = mongoose.Schema({});
+
+ValueSchema.statics.staticMethod = function() {
+    return 'static method works';
+}
+
+ValueSchema.methods.instanceMethod = function() {
+    return 'instance method works';
+}
+
+const Value = mongoose.model('value', ValueSchema);
+
+console.log(Value.staticMethod());
+
+const obj = new Value();
+console.log(obj.instanceMethod());
+
