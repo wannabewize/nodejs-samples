b17da78c9ed8738b2ab82b7bd3adc389c4418610
wannabewize <wannabewize@gmail.com>
2017-04-27 01:28:23 +0900

    promise example update

--- 03.FlowControl/Promise/promise_task.js
+++ 03.FlowControl/Promise/promise_task.js
@@ -8,31 +8,31 @@ function task(success) {
             // 결과를 전달하기 위한 파라미터 설정
             resolve('Success');
         }
-            else {
-                  // 비동기 태스크가 실패(reject)하면 reject 콜백 실행.
-                  // 에러 정보를 파라미터로 전달한다.
-                  reject('Error');
-            }
-      });
+        else {
+            // 비동기 태스크가 실패(reject)하면 reject 콜백 실행.
+            // 에러 정보를 파라미터로 전달한다.
+            reject('Error');
+        }
+    });
 }
 
 // Promise를 사용하는 태스크
 task(true).then((result) => {
-      console.log('첫 번째 비동기 태스크 성공 : ', result);
+    console.log('첫 번째 비동기 태스크 성공 : ', result);
 }, (error) => {
-      console.log('첫 번째 비동기 태스크 실패 : ', error);
+    console.log('첫 번째 비동기 태스크 실패 : ', error);
 });
 
 // reject 되는 프라미스
 task(false).then((result) => {
-      console.log('두 번째 비동기 태스크 성공 : ', result);
+    console.log('두 번째 비동기 태스크 성공 : ', result);
 }, (error) => {
-      console.log('두 번째 비동기 태스크 실패 : ', error);
+    console.log('두 번째 비동기 태스크 실패 : ', error);
 });
 
 // catch를 이용해서 실패시의 콜백을 등록할 수 있다.
 task(false).then(result => {
-      console.log('세 번째 비동기 태스크 성공 :', result);
+    console.log('세 번째 비동기 태스크 성공 :', result);
 }).catch(failure => {
-      console.log('세 번째 비동기 태스크 실패 :', failure);
+    console.log('세 번째 비동기 태스크 실패 :', failure);
 });
