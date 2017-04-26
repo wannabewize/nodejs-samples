4222990ef352952ef1a2087631181a8ee9cb1f83
Jaehoon Lee <wannabewize@gmail.com>
2017-04-26 22:29:07 +0900

    연속된 비동기 태스크 예제

--- 03.FlowControl/Promise/promise_series.js
+++ 03.FlowControl/Promise/promise_series.js
@@ -1,60 +1,59 @@
 /**
  * 비동기 태스크를 연속 동작시키기
- * Task1, Task2는 난수를 하나 만들고, Task3는 두 값을 더한다.
+ * Task1의 결과를 Task2에 전달, Task2의 결과를 Task3전달한다.
  */
 
 // 비동기 태스크 1. 0 ~ 9 사이의 난수 결과
-function task1(arg) {
+function task1(success) {
     console.log('Task1 started');
     const taskResult = Math.round(Math.random() * 10);
     return new Promise((resolve, rejected) => {
         setTimeout(() => {
             console.log('Task1 Finished with', taskResult);
-            var success = arg;
             success ? resolve(taskResult) : rejected('Task1 Failure');
         }, 1000)
     });
 };
 
 // 비동기 태스크 2. 10 ~ 19 사이의 난수 결과
-function task2(result, arg) {
-    console.log('Task2 started with', result);
+function task2(previousTaskResult, success) {
+    console.log('Task2 started with', previousTaskResult);
     // 두 번째 태스크의 결과 : 10 ~ 19 사이의 난수
     const taskResult = Math.round(Math.random() * 10) + 10;
     
     return new Promise((resolve, rejected) => {
         setTimeout(() => {            
-            var success = arg;
-            console.log('Task2 Finished with', taskResult);
-            success ? resolve(taskResult) : rejected('Task2 Failure');
+            const nextResult = [previousTaskResult, taskResult]
+            console.log('Task2 Finished with', nextResult);
+            success ? resolve(nextResult) : rejected('Task2 Failure');
         }, 1500)
     });
 }
 
-// 비동기 태스크 3. 결과는 입력된 두 값의 합
-function task3(result1, result2, arg) {
-    console.log('Task3 started with', result1, result2);
-    const taskResult = result1 + result2;
+// 비동기 태스크 3. 20 ~ 29 사이의 난수 결과
+function task3(previousTaskResult, success) {
+    console.log('Task3 started with', previousTaskResult);
+    const taskResult = Math.round(Math.random() * 10) + 20;
     
     return new Promise((resolve, rejected) => {
         setTimeout(() => {            
             console.log('Task3 Finished with', taskResult);
-            var success = arg;
-            success ? resolve(taskResult) : rejected('Task3 Failure');
+            const nextResult = previousTaskResult.concat(taskResult);
+            success ? resolve(nextResult) : rejected('Task3 Failure');
         }, 1500)
     });
 }
 
-function example1(success) {
+function runSeriesTask1() {
     // Task1, Task2, Task3 연속 호출
-    task1(success).then(task1Result => {
+    task1(true).then(task1Result => {
         console.log('Task1 Success');
 
         task2(task1Result, true).then(task2Result => {
             console.log('Task2 Success');
 
-            task3(task1Result, task2Result, true).then(task3success => {
-                console.log('Task3 Success');
+            task3(task2Result, true).then(task3success => {
+                console.log('All Tasks Done with', task3success);
             }, task3failure => {
                 console.log('Task3 Failure');
             });
@@ -66,26 +65,35 @@ function example1(success) {
     });
 }
 
-// catch를 이용한 태스크 실행
-function example2() {
-    // Task1, Task2, Task3 연속 호출
-    task1(true).then(task1Result => {
-        console.log('Task1 Success');
-        task2(task1Result, true).then(task2Result => {
-            console.log('Task2 Success');
-
-            task3(task1Result, task2Result, true).then(task3success => {
-                console.log('Task3 Success');
-            }).catch(task3failure => {
-                console.log('Task3 Failure');
-            });
-        }).catch(task2failure => {
-            console.log('Task2 Failure');
-        });
-    }).catch(task1failure => {
+function runSeriesTask2() {
+    task1(true).then( task1Result => {
+        return task2(task1Result, true);
+    }, task1Failure => {
+        console.log('Task1 Failure');
+    }).then( task2Result => {
+        return task3(task2Result, true);
+    }, task2failure => {
         console.log('Task2 Failure');
+    }).then( task3Result => {
+        console.log('All Task Done with', task3Result)
+    }, task3Failure => {
+        console.log('Task3 Failure');
     });
 }
 
-// example1(true);
-example2(true);
+function runSeriesTask3() {
+    task1(true).then( task1Result => {
+        return task2(task1Result, true);
+    }).then( task2Result => {
+        return task3(task2Result, true);
+    }).then( task3Result => {
+        console.log('All Task Done with', task3Result);
+    }).catch( taskError => {
+        console.log('Task Error!', taskError);
+    });
+}
+
+
+// runSeriesTask1(true);
+// runSeriesTask2(true);
+runSeriesTask3(true);
--- 03.FlowControl/README.md
+++ 03.FlowControl/README.md
@@ -18,6 +18,11 @@ Promise에 대한 예제
 - asyncTask : Promise 사용 방식을 익히기 위한 예제. 비동기 태스크를 생성하고, 성공 콜백/실패 콜백 사용하기.
 - promise : Promise 객체 생성과 동작 방법 
 - promise_task : Promise를 이용하는 비동기 태스크 만들기
-- promise_series : 연속적으로 Promise 사용하기
+- promise_series : 연속적으로 Promise 사용하기. 이전 비동기 태스크의 결과를 다음 태스크에 전달한다.
 - promise_race : 다수의 비동기 태스크 경쟁시키기.
 - promise_all : 다수의 비동기 태스크 실행.
+
+## Await
+
+ES7에 추가된 await-async를 이용한 비동기 태스크 작성 방법. Promise를 먼저 익혀야한다.
+
