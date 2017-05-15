09a556278b4ec5a02e8cd152530f7e5d06759de0
Jaehoon Lee <wannabewize@gmail.com>
2017-05-15 13:41:11 +0900

    Await Example

---
+++ 03.FlowControl/Await/await2.js
@@ -0,0 +1,32 @@
+function randomTask() {
+    return new Promise((resolve, reject) => {
+            const taskResult = Math.round(Math.random() * 10);
+            setTimeout(() => {
+                resolve(taskResult);
+            }, 1000);
+    });
+}
+
+
+function addTask(arg1, arg2) {
+    return new Promise( (resolve, reject) => {
+        const result = arg1 + arg2;
+        setTimeout(()=>{
+            resolve(result);
+        }, 1000);
+    });
+}
+
+async function doIt() {
+    try {
+        let r1 = await randomTask();
+        let r2 = await randomTask();
+        let sum = await addTask(r1, r2);
+        console.log('Random Numbers : ', r1, r2);
+        console.log('Sum =', sum);
+    } catch (error) {
+        console.log('Task Failure', error);
+    }
+}
+
+doIt();
