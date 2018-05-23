fed24244a29569997b12044df1fc555747514eeb
wannabewize <wannabewize@gmail.com>
2018-05-23 16:35:58 +0900

    네임스페이스, 룸 예제

--- 09.Realtime/Socket.io/Connection/server.js
+++ 09.Realtime/Socket.io/Connection/server.js
@@ -7,6 +7,8 @@ const io = require('socket.io')(3000);
 
 io.on('connection', (socket) => {
    console.log('클라이언트 연결');
+   // console.log('클라이언트 :', socket);
+   console.log('클라이언트 id :', socket.id);
 
    // 원격 호스트 접속 종료 이벤트
    socket.on('disconnect', () => {
--- 09.Realtime/Socket.io/Namespace/server.js
+++ 09.Realtime/Socket.io/Namespace/server.js
@@ -1,36 +1,37 @@
 const express = require('express');
 const http = require('http');
 
-var app = express();
-var server = http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 server.listen(3000);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
    res.sendFile(__dirname + '/index.html');
 });
 
-var io = require('socket.io')(server);
-io.on('connection', function (socket) {
+const io = require('socket.io')(server);
+io.on('connection', (socket) => {
    console.log('클라이언트 접속');
 
-   socket.on('message', function (data) {
+   socket.on('message', (data) => {
       console.log('message from client : ', data['message']);
       socket.emit('echo', data);
    });
 
-   socket.on('disconnect', function () {
+   socket.on('disconnect', () => {
       console.log('Disconnected');
    });
 });
 
-var is = process.stdin;
+// 키보드 입력
+const is = process.stdin;
 
 // System Namespace
 var system = io.of('/system');
-system.on('connection', function (socket) {
+system.on('connection', (socket) => {
    console.log('emergency namespace connected');
 
-   is.on('data', function (chunk) {
+   is.on('data', (chunk) => {
       console.log('emergency namespace!');
       socket.emit('message', { msg: chunk.toString() });
    });
