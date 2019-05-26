84393ba4ad5cd8199cf0cc9a006e17ad491f0bdf
wannabewize <wannabewize@gmail.com>
2019-05-27 02:40:14 +0900

    업데이트

--- 09.Persistence/MongoDB/Basic/connection.js
+++ 09.Persistence/MongoDB/Basic/connection.js
@@ -1,12 +1,12 @@
 const MongoClient = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017/example';
 
-MongoClient.connect(url, (err, database) => {
+MongoClient.connect(url, {useNewUrlParser: true}, (err, database) => {
    if (err) {
       console.log('MongoDB 연결 실패');
       return;
    }
-   console.log("MongoDB 연결 성공");
+   console.log('MongoDB 연결 성공');
 
    // 연결 종료하기
    database.close();
