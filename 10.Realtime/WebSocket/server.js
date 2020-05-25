8940d6c8464a59e944ae75f86341c9df0a03370e
wannabewize <wannabewize@limefriends.com>
2020-05-25 18:34:16 +0900

    웹 소켓 예제 업데이트

--- 10.Realtime/WebSocket/server.js
+++ 10.Realtime/WebSocket/server.js
@@ -1,11 +1,17 @@
-const WebSocket = require('ws');
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
 
-const wss = new WebSocket.Server({ port: 3000 });
-
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
+
+app.ws('/', function(ws, req) {
+  ws.on('message', (message) => {
+    console.log('received:', message);
+
+    ws.send(message);
+  });
+});
+
+app.listen(3000);
