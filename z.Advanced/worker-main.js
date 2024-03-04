639da9fed9b8398b6f1d47ad3d4b526b70f37d10
Lee Jaehoon <wannabewize@gmail.com>
2024-03-04 19:04:55 +0900

    wip

--- z.Advanced/worker-main.js
+++
@@ -1,9 +0,0 @@
-const { Worker, isMainThread, parentPort } = require('node:worker_threads');
-
-
-console.log('main thread');
-const worker = new Worker('./worker.js');
-worker.on('message', msg => {
-    console.log('message from worker :', msg);
-})
-worker.postMessage('Hello');
--- z.Advanced/worker.js
+++
@@ -1,8 +0,0 @@
-const { parentPort } = require('worker_threads');
-
-console.log('worker thread');
-parentPort.on('message', value => {
-    console.log('message from parent', value);
-    parentPort.postMessage('Hi');
-    parentPort.close();
-});
